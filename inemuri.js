f8833097f9c40c92dfb542c892fe62332c3dbac7
StarLith <hommerclod@gmail.com>
2023-11-27 22:55:08 +0200

    v.2.3 add gmail service

--- inemuri.js
+++ inemuri.js
@@ -2,6 +2,7 @@ const Discord = require("./bot/discord")
 const Telegram = require("./bot/telegram")
 const Translator = require("./bot/translator")
 const Spreadsheet = require("./bot/spreadsheet")
+const Gmail = require("./module/gmail/gmail")
 const { intro } = require("./awesome/message")
 const { _error, alarm } = require("./shared/utility")
 
@@ -12,9 +13,10 @@ async function START() {
         // await Translator.launch()
         await Discord.launch()
         await Telegram.launch()
+        await Gmail.launch()
     } catch (error) {
         _error(error.message)
-        await alarm(error.message)
+        alarm(`ERROR | MAIN_LAUNCH | ${error.message}`)
     }
 }
 
